8648265b4897e86de7a31bb4028d8840bd8f471f
WeijiangChen <ourweijiang@gmail.com>
2018-10-04 13:35:51 +0800

    修改日志

--- gunicorn.py
+++ gunicorn.py
@@ -3,17 +3,19 @@ import os
 import multiprocessing
 
 path_of_current_dir = os.getcwd()
-os.mkdir('%s/logs' % path_of_current_dir)
+log_dir = '%s/logs' % path_of_current_dir
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
 
 _file_name = "gunicron"
 
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:7000"
 workers = 1
 worker_class = "egg:meinheld#gunicorn_worker"
 timeout = 100
 
 loglevel = 'info'
 
-pidfile = '%s/logs/%s.pid' % (path_of_current_dir, _file_name)
-errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
-accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
+pidfile = '%s/%s.pid' % (log_dir, _file_name)
+errorlog = '%s/%s_error.log' % (log_dir, _file_name)
+accesslog = '%s/%s_access.log' % (log_dir, _file_name)
